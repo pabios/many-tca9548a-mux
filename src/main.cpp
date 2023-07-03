e4f7d380571c077ffc2b2535471b68e7c7f5ca36
pabios <pabios.pabiosoft@gmail.com>
2023-07-03 03:11:47 +0200

    [init](first):initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+void setup() {
+// write your initialization code here
+Serial.begin(9600);
+}
+
+void loop() {
+// write your code here
+Serial.println("Bonjour le monde");
+}
