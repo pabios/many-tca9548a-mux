ef18cf0badef2567093db065997a43a20a1f6ceb
pabios <pabios.pabiosoft@gmail.com>
2023-07-05 23:50:29 +0200

    [Refactor](Oled):use of plug-in screens

--- src/main.cpp
+++ src/main.cpp
@@ -25,24 +25,31 @@ Adafruit_SSD1306 display7_74(128, 64, &Wire, -1);
 Adafruit_SSD1306 tca1[] = {display0_70,display1_70,display2_70,display3_70,display4_70,display5_70,display6_70,display7_70};
 Adafruit_SSD1306 tca2[] = {display0_74,display1_74,display2_74,display3_74,display4_74,display5_74,display6_74,display7_74};
 //
+Element oled2_70(0x70,4,tca1[2]);
+Element oled7_70(0x70,4,tca1[7]);
 Element oled4_70(0x70,4,tca1[4]);
 Element oled5_70(0x70,5,tca1[5]);
 
-Element oled4_74(0x74,4,tca2[4]);
+Element oled2_74(0x74,4,tca2[2]);
+Element oled7_74(0x74,4,tca2[7]);
 //-----------------------------------------------------
 void setupScreen(){
     Wire.begin();
-    oled4_70.setupOled();
-    oled5_70.setupOled();
+    oled2_70.setupOled();
+    oled7_70.setupOled();
     //
-    oled4_74.setupOled();
+    oled2_74.setupOled();
+    oled7_74.setupOled();
 }
 void loopScreen(){
-    oled4_70.send("hello 70-4");
+    oled2_70.send("Screen 2 tca 1 - 0x70");
     delay(2000);
-    oled5_70.send("70-5 hello");
+    oled7_70.send("Screen 7 tca 1 - 0x70");
     delay(2000);
-    oled4_74.send("4-74 bonjour");
+    //
+    oled2_74.send("Screen 2 tca 2 - 0x74");
+    delay(2000);
+    oled7_74.send("Screen 7 tca 2 - 0x74");
     delay(2000);
 }
 //-----------------------------------------------------
